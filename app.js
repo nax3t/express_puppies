caf9c33043f93f96928d701dc87dd8d28a5ba9bd
ian schoonover <ian@ians-MacBook-Pro.local>
2015-09-02 12:06:27 -0700

    Add CR-- capabilities

---
+++ app.js
@@ -0,0 +1,67 @@
+// boilerplate code
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override');
+
+// set view engine
+app.set('view engine', 'ejs');
+// set static directory to public
+app.use(express.static('public'));
+// use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+// use method-override
+app.use(methodOverride('_method'));
+
+// assign puppies to array of objects
+var puppies = [
+		{
+			id: 1,
+			name: "Billy",
+			age: 2,
+			img: "http://cdn.sheknows.com/articles/2013/04/Puppy_2.jpg"
+		},
+		{
+			id: 2,
+			name: "Joey",
+			age: 1,
+			img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTDSLuUGcahRMlzSnurC5f1kLExUfTrnrGTt82753XgdhsJ_OTf"
+		}
+	]
+
+// "GET" request to "/" path runs the function below
+app.get('/', function (req, res) {
+	res.render('puppies/index', {puppies: puppies});
+});
+
+// list all puppies
+app.get('/puppies', function (req, res) {
+	res.render('puppies/index', {puppies: puppies});
+});
+
+// new puppy form
+app.get('/puppies/new', function (req, res) {
+	res.render('puppies/new');
+});
+
+// post new puppy
+app.post('/puppies', function (req, res) {
+		var name = req.body.name,
+				age = req.body.age,
+				img = req.body.img,
+				id = puppies[puppies.length-1].id;
+
+		puppies.push({id: id += 1, name: name, age: age, img: img});
+		res.redirect('/puppies/'+id);
+});
+
+// show puppy by id
+app.get('/puppies/:id', function (req, res) {
+	var puppy = puppies[req.params.id-1];
+	res.render('puppies/show', {puppy: puppy});
+});
+
+// start the server
+app.listen(3000, function () {
+	console.log('Starting a server on localhost:3000');
+});
